9e9bef4fe87b075a18a56358293649df57b83ccc
Matthias Täschner <27773321+foerster-finsternis@users.noreply.github.com>
2025-09-09 13:20:36 +0200

    Add doc

--- app/analysis/max.py
+++ app/analysis/max.py
@@ -2,5 +2,13 @@ import pandas as pd
 
 
 def compute(series: pd.Series) -> float:
-    
+    """Compute the max of a pandas Series.
+
+    Args:
+        series (pd.Series): Input series to compute the max.
+
+    Returns:
+        float: The max of the series.
+
+    """
     return series.max()
